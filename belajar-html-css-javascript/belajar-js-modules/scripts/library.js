793d2f0cc9247f19a2c47f0998ad1160e96eaad1
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-08-04 22:37:09 +0700

    commit belajar js modules "foundamental js modules"

---
+++ belajar-html-css-javascript/belajar-js-modules/scripts/library.js
@@ -0,0 +1,9 @@
+function goodNight (name){
+    console.info(`hallo ${name}, selemat malam`);
+}
+
+function goodMorning (name){
+    console.info(`hallo ${name}, selemat pagi`);
+}
+
+const name = ("luffy")
