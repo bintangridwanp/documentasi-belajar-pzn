02839ec040cb30c35021ba4429b922991f4b8e9a
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-08-18 20:32:33 +0700

    belajar java dasar 3

--- belajar-java/belajar-java-dasar/src/TipeDataString.java
+++ belajar-java/belajar-java-dasar/src/TipeDataString.java
@@ -1,16 +1,28 @@
 public class TipeDataString {
     public static void main(String[] args) {
 
+        // Deklarasi variabel tipe data String untuk menyimpan nama awal
         String namaAwal = "Luffy";
+
+        // Deklarasi variabel tipe data String untuk menyimpan nama tengah
         String namaTengah = "D";
+
+        // Deklarasi variabel tipe data String untuk menyimpan nama akhir
         String namaAkhir = "Monkey";
 
+        // Menggabungkan namaAwal, namaTengah, dan namaAkhir menjadi satu string dengan spasi di antaranya
         String namaLengkap = namaAwal + " " + namaTengah + " " + namaAkhir;
 
+        // Mencetak nilai variabel namaAwal
         System.out.println(namaAwal);
+
+        // Mencetak nilai variabel namaTengah
         System.out.println(namaTengah);
+
+        // Mencetak nilai variabel namaAkhir
         System.out.println(namaAkhir);
 
+        // Mencetak nama lengkap yang merupakan hasil penggabungan namaAwal, namaTengah, dan namaAkhir
         System.out.println(namaLengkap);
 
     }
