7094a5c44ff635380d13270844709583406c88c9
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-09-25 23:21:03 +0700

    belajar java oop 1

---
+++ belajar-java/belajar-java-oop/src/PersonApp.java
@@ -0,0 +1,16 @@
+public class PersonApp {
+    public static void main(String[] args) {
+
+        var Person1 = new Person();
+
+        Person Person2 = new Person();
+
+        Person Person3;
+        Person3 = new Person();
+
+        System.out.println(Person1);
+        System.out.println(Person2);
+        System.out.println(Person3);
+
+    }
+}
