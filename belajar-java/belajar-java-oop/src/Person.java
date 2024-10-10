ce065efb9089c7c8f9499a48de4f9c758cbbd4db
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-10-10 12:08:50 +0700

    belajar java oop 2

--- belajar-java/belajar-java-oop/src/Person.java
+++ belajar-java/belajar-java-oop/src/Person.java
@@ -3,4 +3,23 @@
         int age;
         String address;
         final String country = "indonesia";
+
+        Person(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        Person(String name, int age) {
+            this(name,  age, null);
+        }
+
+        Person(String forName) {
+            this(null, 0);
+        }
+
+        void hello(String paramName){
+            System.out.println("Hello " + paramName + " myName " + name);
+        }
+
     } 
--- belajar-java/belajar-java-oop/src/PersonApp.java
+++ belajar-java/belajar-java-oop/src/PersonApp.java
@@ -1,20 +1,23 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person();
-        person1.name = "luffy";
-        person1.age = 19;
-        person1.address = "Bandung";
-
+        var person1 = new Person("luffy", 19, "east blue");
         System.out.println(person1.name);
         System.out.println(person1.age);
         System.out.println(person1.address);
         System.out.println(person1.country);
 
-        Person Person2 = new Person();
+        System.out.println("");
+
+        Person Person2 = new Person("zoro", 21, "east blue");
+        System.out.println(Person2.name);
+        Person2.hello("nami");
+
+        System.out.println("");
 
         Person Person3;
-        Person3 = new Person();
+        Person3 = new Person("nico robin", 23, "west blue");
+        System.out.println(Person3.name);
 
 
     }
