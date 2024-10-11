ab26f58339676730f7490a906aca0647e5946c95
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-10-11 21:59:05 +0700

    belajar java oop 3

---
+++ belajar-java/belajar-java-oop/src/Father.java
@@ -0,0 +1,9 @@
+public class Father {
+    String name;
+
+    void sapaHalo(String name){
+        System.out.println(" Hallo "+name + " saya bapak " + this.name);
+    }
+
+
+}
---
+++ belajar-java/belajar-java-oop/src/ParentsApp.java
@@ -0,0 +1,14 @@
+public class ParentsApp {
+    public static void main(String[] args) {
+
+    var father  = new Father();
+    father.name = "tono";
+
+    father.sapaHalo("gorosei");
+
+    var mother = new Mother();
+    mother.name = "mulyono";
+
+    mother.sapaHalo("sri");
+    }
+}
